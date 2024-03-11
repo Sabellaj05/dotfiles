5d2267c04d0fc24ca15ffa4e2ef057a540aab1c1
Don <manumancuso05@gmail.com>
2024-03-11 03:02:48 -0300

    Fixed autostart script

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-feh --bg-scale /home/don/Pictures/Wallpapers/wall-anime.jpeg &
+feh --bg-scale /home/don/Pictures/Wallpapers/wall-japan.jpeg &
 picom --config /home/don/.config/picom/picom.conf &
-dunst --config /home/don/.config/dunst/dunstrc
+dunst --config /home/don/.config/dunst/dunstrc &
