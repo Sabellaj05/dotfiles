487279ced0d40be513244d51b55ee9de6b56d419
Don <manumancuso05@gmail.com>
2024-04-10 21:43:17 -0300

    [Neovim]: Added hybrid line number instead of zero in cursor,

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -1,7 +1,10 @@
 
 -- relative numbers :p 
-vim.opt.relativenumber = true      
-vim.opt.cursorline = true
+vim.opt.relativenumber = true
+vim.opt.number = true              -- number of line in highlited cursor
+-- vim.opt.hybridlinenumber = true      
+--
+vim.opt.cursorline = true          -- line following cursor
 
 -- spaces and tabs
 vim.opt.tabstop = 4
