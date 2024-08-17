a7dba3999960cf3a11ae8014d053aa3b4ec55c8e
Don <manumancuso05@gmail.com>
2024-08-16 23:59:38 -0300

    [Nvim][Alacritty]: Changed cursor to blink in alacritty, added left spaceing in nvim number line

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -6,6 +6,9 @@ vim.opt.number = true              -- number of line in highlited cursor
 --
 vim.opt.cursorline = true          -- line following cursor
 
+-- left spacing for numberline
+vim.o.signcolumn = "yes:2"
+
 -- spaces and tabs
 -- vim.opt.tabstop = 4
 -- vim.opt.tabstop = 4
