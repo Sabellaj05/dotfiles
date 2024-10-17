c55d66380414762dd38e6114788e9d98b3c97865
Don <manumancuso05@gmail.com>
2024-10-17 17:02:21 -0300

    [nvim] messing around

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,7 +1,7 @@
 
 -- space key as leader
 vim.g.mapleader = " "
-vim.g.maplocalleader = " " 
+vim.g.maplocalleader = " "
 
 -- Basic clipboard interaction
 vim.keymap.set({'n', 'x'}, '<leader>y', '"+y') -- copy
@@ -9,6 +9,10 @@ vim.keymap.set({'n', 'x'}, '<leader>p', '"+p') -- paste
 
 -- Clear search `/`
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
+-- terminal h and v  -- wip
+-- vim.keymap.set('n', '<leader>th', ':hsplit terminal')
+-- vim.keymap.set('n', '<leader>tv', ':vsplit term://bash<CR>')
 
 
 
